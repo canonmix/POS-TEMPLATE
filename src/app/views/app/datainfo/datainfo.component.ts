77f0e2babf6c0e2e29eac297b67a589ce34c8857
Thawit <thawit.intrawut@gmail.com>
2021-06-24 19:29:59 +0700

    เตรียมไฟล์ Template เพื่อใช้จัดทำงานตามไฟล์ "POS_MAIN_หน้าหลัก.jpeg"

--- src/app/views/app/datainfo/datainfo.component.ts
+++ src/app/views/app/datainfo/datainfo.component.ts
@@ -1,13 +1,48 @@
 import { Component} from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-blank-page',
-  templateUrl: './datainfo.component.html'
+  templateUrl: './datainfo.component.html',
+  styleUrls: ['./datainfo.component.scss'],
 })
 export class datainfoComponent  {
 
-  constructor() { }
+  public inputData: any;
+  public disabled: any;
+  public labelPosition: any;
+  public indeterminate: any;
+  public checked: any;
 
+  campaignOne: FormGroup;
+  campaignTwo: FormGroup;
 
+  toppings = new FormControl();
+
+  toppingList: string[] = [
+    'Extra cheese',
+    'Mushroom',
+    'Onion',
+    'Pepperoni',
+    'Sausage',
+    'Tomato',
+  ];
+
+  constructor(public dialog: MatDialog) {
+    const today = new Date();
+    const month = today.getMonth();
+    const year = today.getFullYear();
+
+    this.campaignOne = new FormGroup({
+      start: new FormControl(new Date(year, month, 13)),
+      end: new FormControl(new Date(year, month, 16)),
+    });
+
+    this.campaignTwo = new FormGroup({
+      start: new FormControl(new Date(year, month, 15)),
+      end: new FormControl(new Date(year, month, 19)),
+    });
+  }
 
 }
