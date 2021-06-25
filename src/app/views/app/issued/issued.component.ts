b9f8471be4d2358d2075db41c70b97a7ee95ee5a
Thawit <thawit.intrawut@gmail.com>
2021-06-25 19:27:52 +0700

    ขึ้น Path Menu "พิมพ์ใบกำกับภาษี" ใหม่

---
+++ src/app/views/app/issued/issued.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-blank-page',
+  templateUrl: './issued.component.html'
+})
+export class IssuedPageComponent  {
+
+  constructor() { }
+
+
+
+}
